b58d6c3de0774d17350a3f832b14abb99e4e5f83
Bruno Ricardo de Oliveira <brunoricardo@brunoricardo.me>
2024-02-29 03:25:08 -0500

    Initial Commit

---
+++ VisionOsSandBox/VisionOsSandBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOsSandBoxApp.swift
+//  VisionOsSandBox
+//
+//  Created by user254616 on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOsSandBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
